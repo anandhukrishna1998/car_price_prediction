2611b2525e09f3c161e1e75845b0a405e948d88b
Garima5270 <morigarima@gmail.com>
2024-03-28 13:26:09 +0100

    updated sql folder

---
+++ sql_app/repositories.py
@@ -0,0 +1,37 @@
+from sqlalchemy.orm import Session
+from datetime import date
+from typing import List
+from datetime import datetime
+from . import models, schemas
+
+
+class CarDetailsRepo:
+    def create(db: Session, segement: schemas.car_details):
+        db_item = models.vehicle_details(year=segement.year, 
+                                            km_driven=segement.km_driven,
+                                            seats=segement.seats,
+                                            mileage=segement.mileage, 
+                                            engine=segement.engine,
+                                            max_power=segement.max_power,
+                                            car_company_name=segement.car_company_name,
+                                            fuel=segement.fuel,
+                                            transmission=segement.transmission,
+                                            owner=segement.owner,
+                                            predicted_price=segement.predicted_price,
+                                            timestamp=segement.timestamp
+                                            )
+        db.add(db_item)
+        db.commit()
+        db.refresh(db_item)
+        return db_item
+
+    def get_by_date_range(db: Session, start_date: date, end_date: date) -> List[schemas.car_details]:
+        start_datetime = datetime.combine(start_date, datetime.min.time())
+        end_datetime = datetime.combine(end_date, datetime.max.time())
+        return db.query(models.vehicle_details).filter(
+            models.vehicle_details.timestamp >= start_datetime,
+            models.vehicle_details.timestamp <= end_datetime
+        ).all()
+        
+
+    
